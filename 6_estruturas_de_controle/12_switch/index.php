f92176c9a45135f6f1e30ac6516a288af501057d
luizvelasco <luizvelasco@gmail.com>
2023-05-25 08:24:37 -0300

    fim cap

--- 6_estruturas_de_controle/12_switch/index.php
+++ 6_estruturas_de_controle/12_switch/index.php
@@ -1,2 +1,14 @@
 <?php
 
+$x = 5;
+
+switch ($x){
+    case 0:
+        echo "X = 0";
+        break;
+    case 1:
+        echo "X = 1";
+        break;
+    default:
+        echo "x não é = a nenhum valor informado";
+}

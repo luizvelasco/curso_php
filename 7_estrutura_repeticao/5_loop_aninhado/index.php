478f63dba31118168ce1c7f3a10a8ba06194c5aa
luizvelasco <luizvelasco@gmail.com>
2023-05-25 08:58:46 -0300

    loop_aninhado

---
+++ 7_estrutura_repeticao/5_loop_aninhado/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$i = 1;
+
+while ($i <= 10) {
+    echo "Loop externo $i <br>";
+
+    $j = 1;
+
+    while ($j <=5){
+        echo "Loop interno $j <br>";
+        $j++;
+    }
+    $i++;
+}

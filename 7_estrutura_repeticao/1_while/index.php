f95e6e13b5cd26ece7dea3ef19e11ee2532f5101
luizvelasco <luizvelasco@gmail.com>
2023-05-25 08:34:51 -0300

    while

---
+++ 7_estrutura_repeticao/1_while/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$x = 0;
+
+while ($x < 10) {
+    echo $x . "<br>";
+
+    $x++;
+}
+
+echo "Continuando código...<br>";
+
+$y = 0;
+
+while ($y <= 10) {
+    echo $y . "<br>";
+
+    $y+=2;
+}

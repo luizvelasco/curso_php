b3df1fdd22b4706416331a13dbdf59229302c960
luizvelasco <luizvelasco@gmail.com>
2023-05-27 17:36:58 -0300

    parametros

---
+++ 9_funcoes/6_parametros/index.php
@@ -0,0 +1,12 @@
+<?php
+
+function velocidadeMaxima($vel) {
+    echo "O carro atinge a velocidade máxima de $vel km/h <br>";
+}
+
+velocidadeMaxima(200);
+velocidadeMaxima(400);
+
+$velocidade = 225;
+
+velocidadeMaxima($velocidade, "rwaw");

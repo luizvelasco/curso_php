6a875ca426a22a65e25eb106f78ca56555de2c29
Luiz Velasco <luizvelasco@gmail.com>
2023-05-29 19:19:30 -0300

    exer

--- 10_strings/14_substr/index.php
+++ 10_strings/14_substr/index.php
@@ -6,3 +6,13 @@ $minha = substr($str, 10, 5);
 
 echo "$str<br>";
 echo "$minha<br>";
+
+$str2 = "Testando esta string abc";
+
+$novaString = substr($str2, 9); //omitir cumprimento, pegar até o fim
+
+echo $novaString . "<br>";
+
+$novaString2 = substr($str2, -3); // comprimento negativo = remover do último índice
+
+echo $novaString2;
---
+++ 10_strings/15_exer_42/index.php
@@ -0,0 +1,7 @@
+<?php 
+
+$frase = "Quem mexeu no meu queijo? Ele estava aqui em cima";
+
+$queijo = substr($frase, 18, 6);
+
+echo "{$queijo} <br> ";

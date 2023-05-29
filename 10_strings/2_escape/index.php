c711e65093dc75787317601593e07dd7287dc6a1
Luiz Velasco <luizvelasco@gmail.com>
2023-05-29 15:25:30 -0300

    escape

---
+++ 10_strings/2_escape/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+header('Content-type: text/plain');
+
+//Pular linha
+echo "Isso aqui vai ficar na primeira linha \nE issno na segunda linha \n";
+
+// tab
+echo "Testando o tab \t aqui \n";
+
+// barra invertida
+echo "Barra invertida \\ \n";
+
+// dolar
+echo "Imprimindo o Dólar \$teste";

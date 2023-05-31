e636346b9881d9d80a3e2e05a893f428a6104bfd
luizvelasco <luizvelasco@gmail.com>
2023-05-31 08:21:20 -0300

    range

---
+++ 11_arrays/2_adicionando_ao_fim_array/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$arr = [1,2,3];
+
+$arr[] = 4;
+
+var_dump($arr);
+echo "<br>";
+
+$arr[] = 5;
+
+var_dump($arr);
+echo "<br>";
---
+++ 11_arrays/3_range/index.php
@@ -0,0 +1,6 @@
+<?php
+
+$arr = range(1,10);
+
+var_dump($arr);
+echo "<br>";

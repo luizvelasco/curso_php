1463c1b3a2c33d77f383c4bc42f2afa9fd5e470e
Luiz Velasco <luizvelasco@gmail.com>
2023-06-14 19:05:45 -0300

    create

--- 17_agenda/create.php
+++ 17_agenda/create.php
@@ -1,7 +1,20 @@
 <?php 
-
     include_once("templates/header.php");
-    include_once("templates/footer.php");
-
 ?>
-    <h1>Criando contato</h1>
+
+<div class="container">
+    <?php include_once("templates/backbtn.html"); ?>
+    <h1 id="main-title">Criar Contato</h1>
+    <form action="<?= $BASE_URL?>config/process.php" method="post">
+        <input type="hidden" name="type" value="create">
+        <div class="form group">
+            <label for="name">Nome do contato:</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
+</div>
+
+<?php
+    include_once("templates/footer.php");
+?>

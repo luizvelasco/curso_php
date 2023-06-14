b44e2e795b4c11be00a97a8279c5e0a421a807af
luizvelasco <luizvelasco@gmail.com>
2023-06-14 08:56:13 -0300

    show

--- 17_agenda/config/process.php
+++ 17_agenda/config/process.php
@@ -5,6 +5,22 @@ session_start();
 include_once("connection.php");
 include_once("url.php");
 
+$id;
+
+if (!empty($_GET)){
+    $id = $_GET['id'];
+}
+
+//retorna o dado de um contato
+if (!empty($id)){
+    $query = "SELECT * FROM contacts WHERE id = :id";
+
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(":id", $id);
+    $stmt->execute();
+    $contact = $stmt->fetch();
+} else {
+//retorna todos os contatos
 $contacts = [];
 
 $query = "SELECT * FROM contacts";
@@ -13,3 +29,5 @@ $stmt = $conn->prepare($query);
 $stmt->execute();
 
 $contacts = $stmt->fetchAll();
+}
+

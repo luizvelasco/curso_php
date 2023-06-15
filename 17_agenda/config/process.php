fc87040d8b0337af490cbe9ffa3a5b81fc4cd24f
Luiz Velasco <luizvelasco@gmail.com>
2023-06-15 16:47:06 -0300

    atualizar

--- 17_agenda/config/process.php
+++ 17_agenda/config/process.php
@@ -34,6 +34,35 @@ if (!empty($data)){
             echo "Erro: $error";
         }
 
+    } elseif ($data["type"] === "edit") {
+        $name = $data["name"];
+        $phone = $data["phone"];
+        $observations = $data["observations"];
+        $id = $data["id"];
+
+        $query = "UPDATE contacts 
+                    SET name = :name,
+                        phone = :phone,
+                        observations = :observations
+                    WHERE id = :id";
+
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(":name", $name);
+        $stmt->bindParam(":phone", $phone);
+        $stmt->bindParam(":observations", $observations);
+        $stmt->bindParam(":id", $id);
+
+        try {
+            $stmt->execute();
+            $_SESSION["msg"] = "Contato atualizado com sucesso!";
+            
+        } catch (PDOException $e) {
+            //erro na conexçao;
+            $error = $e->getMessage();
+            echo "Erro: $error";
+        }
+
     }
 
     //Redirect HOME

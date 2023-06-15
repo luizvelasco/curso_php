7277686ff23ff7fc8d54b6b32cf3f9f6aebeaf4f
Luiz Velasco <luizvelasco@gmail.com>
2023-06-15 20:03:26 -0300

    dao

---
+++ 18_design_patterns/1_dao/dao/CarDAO.php
@@ -0,0 +1,17 @@
+<?php 
+
+    include_once("models/Car.php");
+
+    class CarDAO implements CarDAO {
+
+        public function findAll(){
+
+        }
+
+        public function create(Car $car) {
+            
+        }
+
+    }
+
+?>

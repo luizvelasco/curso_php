f59f36ffdc0edeba5e7f9c5ec44eb440a893d4b9
luizvelasco <luizvelasco@gmail.com>
2023-06-14 21:14:35 -0300

    create

--- 17_agenda/create.php
+++ 17_agenda/create.php
@@ -5,11 +5,19 @@
 <div class="container">
     <?php include_once("templates/backbtn.html"); ?>
     <h1 id="main-title">Criar Contato</h1>
-    <form action="<?= $BASE_URL?>config/process.php" method="post">
+    <form id="create-form" action="<?= $BASE_URL?>config/process.php" method="post">
         <input type="hidden" name="type" value="create">
-        <div class="form group">
+        <div class="form-group">
             <label for="name">Nome do contato:</label>
             <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" required>
+        </div>
+        <div class="form-group">
+            <label for="phone">Telefone do contato:</label>
+            <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone" required>
+        </div>
+        <div class="form-group">
+            <label for="observations">Observações:</label>
+            <textarea type="text" class="form-control" id="observations" name="observations" placeholder="Observações" rows="3"></textarea>
         </div>
         <button type="submit" class="btn btn-primary">Cadastrar</button>
     </form>
